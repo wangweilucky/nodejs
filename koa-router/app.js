7aac40116c43f4906a931c14e37845042f33cafd
wei.wang <wei.wang@magicwindow.cn>
2018-12-20 10:36:33 +0800

    路由

--- koa-router/app.js
+++ koa-router/app.js
@@ -11,17 +11,18 @@ const bodyParser = require('koa-bodyparser')();
 // 0. 添加body解析功能，post请求
 app.use(bodyParser);
 
+// 1. 添加 url-router, 并生成所有的导入controller middleware:
+const controller = require('./router.js');
+
+// 2. 添加router
+app.use(controller());
+
+
 // 1. 打印请求的数据连接
 app.use(async (ctx, next) => {
     console.log(`url: ${ctx.request.method} ${ctx.request.url}`);
     await next();
 });
-
-// 2. 添加 url-router, 并生成所有的导入controller middleware:
-const controller = require('./router.js');
-
-// 3. 添加router
-app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
--- koa-router/controllers/index.js
+++ koa-router/controllers/index.js
@@ -16,7 +16,54 @@ var fn_signin = async(ctx, next) => {
     password = ctx.request.body.password || '';
     console.log(`signin with name: ${name}, password: ${password}`);
     if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+
+
+        const nunjucks = require('nunjucks');
+
+        function createEnv(path, opts) {
+            var
+                autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+                noCache = opts.noCache || false,
+                watch = opts.watch || false,
+                throwOnUndefined = opts.throwOnUndefined || false,
+                env = new nunjucks.Environment(
+                    new nunjucks.FileSystemLoader('views', {
+                        noCache: noCache,
+                        watch: watch,
+                    }), {
+                        autoescape: autoescape,
+                        throwOnUndefined: throwOnUndefined
+                    });
+            if (opts.filters) {
+                for (var f in opts.filters) {
+                    env.addFilter(f, opts.filters[f]);
+                }
+            }
+            return env;
+        }
+        
+        var env = createEnv('views', {
+            watch: true,
+            filters: {
+                hex: function (n) {
+                    return '0x' + n.toString(16);
+                }
+            }
+        });
+        
+        var renderdata = {
+             name: name ,
+              fruits: ['apple', 'o', '1'], 
+              header: '这是头部',
+               body: '这是内容', 
+               footer: '这是尾部'
+            };
+
+        var s = env.render('hello.html', renderdata);
+
+        console.log(s);
+
+        ctx.response.body = s;
     } else {
         ctx.response.body = `<h1>Login failed!</h1>
                             <p><a href="/">Try again</a></p>`;
